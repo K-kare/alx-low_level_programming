41b5c11341acd0e355101e0c9a14c05d4f498f04
K-kare <kamunawangari@gmail.com>
2023-12-01 06:09:40 -0800

    3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,7 +2,13 @@
 #define MAIN_H
 #include <stddef.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
 int _putchar(char c);
+void check_IO_stat(int stat, int fd, char *filename, char mode);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
