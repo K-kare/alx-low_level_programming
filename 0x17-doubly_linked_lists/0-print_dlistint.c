acd8cdf68acd557373aef0838ed91b2050449e73
K-kare <kamunawangari@gmail.com>
2024-02-08 02:44:49 -0800

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,12 +9,13 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int num;
+
 	num = 0;
 	if (h == NULL)
 		return (num);
-	while(h->prev != NULL)
+	while (h->prev != NULL)
 		h = h->prev;
-	while(h != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		num++;
